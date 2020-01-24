a0c92a071b2f13ada0ce5d4d87a174610de91f42
rgavin1 <44215916+rgavin1@users.noreply.github.com>
2020-01-23 20:12:22 -0500

    First Commit

---
+++ src/build/precache-manifest.e78c104d285ac4169fc55ad11ea9100a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82cea571413121e0a11a8cf5bd2e206f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "650d7036a7b9b90c3682",
+    "url": "/static/css/main.091ff9c7.chunk.css"
+  },
+  {
+    "revision": "95cb86e9f30134d1f232",
+    "url": "/static/js/2.6f55f6c8.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.6f55f6c8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "650d7036a7b9b90c3682",
+    "url": "/static/js/main.02981530.chunk.js"
+  },
+  {
+    "revision": "91de3d6c4e87c90b7d90",
+    "url": "/static/js/runtime-main.7ffca68f.js"
+  }
+]);
